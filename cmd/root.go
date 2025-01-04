aa4d2de1e400c2c927de20991ca91ea6b97f861f
Nicholas Bannister-Andrews <nbannist@gmail.com>
2025-01-04 10:57:39 -0500

    update command name; add short and long description

--- cmd/root.go
+++ cmd/root.go
@@ -15,14 +15,12 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "go-cli-weather",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "weather",
+	Short: "A weather app for your cli. Get weather information in your terminal.",
+	Long: `Use it to retrieve and display weather data on your cli.
+	
+	E.g., '% weather --zip=90210'
+	`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -44,7 +42,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go-cli-weather.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.weather.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
@@ -61,10 +59,10 @@ func initConfig() {
 		home, err := os.UserHomeDir()
 		cobra.CheckErr(err)
 
-		// Search config in home directory with name ".go-cli-weather" (without extension).
+		// Search config in home directory with name ".weather" (without extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigType("yaml")
-		viper.SetConfigName(".go-cli-weather")
+		viper.SetConfigName(".weather")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
